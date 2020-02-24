e27f729bdd779944f8a2b26bca19084ea8a20cd2
莜莜 <2465826537@qq.com>
2020-02-24 16:34:05 +0800

    first init jcart store back

---
+++ jcart-store-back/src/test/java/io/ylf/jcartstoreback/JcartStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ylf.jcartstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartStoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
