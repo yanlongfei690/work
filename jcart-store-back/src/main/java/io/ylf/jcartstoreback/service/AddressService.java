d7285c594894288fb153ec2b038339ec38ddf5c3
莜莜 <2465826537@qq.com>
2020-03-03 16:55:06 +0800

    store update

---
+++ jcart-store-back/src/main/java/io/ylf/jcartstoreback/service/AddressService.java
@@ -0,0 +1,19 @@
+package io.ylf.jcartstoreback.service;
+
+import io.ylf.jcartstoreback.po.Address;
+
+import java.util.List;
+
+public interface AddressService {
+
+    Address getById(Integer addressId);
+
+    List<Address> getByCustomerId(Integer customerId);
+
+    Integer create(Address address);
+
+    void update(Address address);
+
+    void delete(Integer addressId);
+
+}
