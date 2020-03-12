6185be622bd1fa532451133febcfc2c5edd5394b
莜莜 <2465826537@qq.com>
2020-03-12 11:32:51 +0800

    fix page

--- jcart-administration-front/viewmodels/order-show.js
+++ jcart-administration-front/viewmodels/order-show.js
@@ -1,56 +1,71 @@
 var app = new Vue({
     el: '#app',
-    data: {orderId: '',
-    customerId: '',
-    customerName: '',
-    status: '',
-    totalPrice: '',
-    rewordPoints: '',
-    createTimestamp: '',
-    updateTimestamp: '',
-    shipMethod: '',
-    shipAddress: '',
-    shipPrice: '',
-    payMethod: '',
-    invoiceAddress: '',
-    invoicePrice: '',
-    comment: '',
-    orderProducts: [],
-    orderHistories: [],
-    orderStatuses: [
-        { value: 0, label: '待处理' },
-        { value: 1, label: '处理中' },
-        { value: 2, label: '待发货' },
-        { value: 3, label: '已发货' },
-        { value: 4, label: '待签收' },
-        { value: 5, label: '已签收' },
-        { value: 6, label: '待支付' },
-        { value: 7, label: '已支付' },
-        { value: 8, label: '取消' },
-        { value: 9, label: '拒绝' },
-        { value: 10, label: '完成' },
-        { value: 11, label: '待评价' },
-        { value: 12, label: '已评价' }
-    ],
-    createHistoryOrderStatus: '',
-    createHistoryCustomerNotified: false,
-    createHistoryComment: ''
-    },
-    mounted(){
-        console.log("view mounted");
+    data: {
+        orderId: '',
+        customerId: '',
+        customerName: '',
+        status: '',
+        totalPrice: '',
+        rewordPoints: '',
+        createTimestamp: '',
+        updateTimestamp: '',
+        shipMethod: '',
+        shipAddress: '',
+        shipPrice: '',
+        payMethod: '',
+        invoiceAddress: '',
+        invoicePrice: '',
+        comment: '',
+        orderProducts: [],
+        orderHistories: [],
+        orderStatuses: [
+            { value: 0, label: '待处理' },
+            { value: 1, label: '处理中' },
+            { value: 2, label: '待发货' },
+            { value: 3, label: '已发货' },
+            { value: 4, label: '待签收' },
+            { value: 5, label: '已签收' },
+            { value: 6, label: '待支付' },
+            { value: 7, label: '已支付' },
+            { value: 8, label: '取消' },
+            { value: 9, label: '拒绝' },
+            { value: 10, label: '完成' },
+            { value: 11, label: '待评价' },
+            { value: 12, label: '已评价' }
+        ],
+        shipMethods: [
+            { value: 0, label: 'EMS' },
+            { value: 1, label: '顺丰' },
+            { value: 2, label: '圆通' },
+            { value: 3, label: '中通' },
+            { value: 4, label: '申通' }
+        ],
+        payMethods: [
+            { value: 0, label: '货到付款' },
+            { value: 1, label: '借记卡' },
+            { value: 2, label: '信用卡' },
+            { value: 3, label: '微信支付' },
+            { value: 4, label: '支付宝' }
+        ],
+        createHistoryOrderStatus: '',
+        createHistoryCustomerNotified: false,
+        createHistoryComment: ''
 
-        var url=new URL(location.href);
-        this.orderId= url.searchParams.get("orderId");
-        if(!this.orderId){
+    },
+    mounted() {
+        console.log('view mounted');
+
+        var url = new URL(location.href);
+        this.orderId = url.searchParams.get("orderId");
+        if (!this.orderId) {
             alert('orderId is null');
-            return ;
+            return;
         }
-            this.getOrderById();
 
+        this.getOrderById();
+        this.getHistoryByOrderId();
     },
-
-    methods:{
-
+    methods: {
         handleCreateOrderHistoryClick() {
             console.log('create order history click');
             this.createOrderHistory();
@@ -118,7 +133,5 @@ var app = new Vue({
                     console.log(error);
                 });
         }
-
     }
-
 })
