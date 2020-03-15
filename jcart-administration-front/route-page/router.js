50d0ad50cfe5f3ff266be19a55019c84ec40cc43
莜莜 <2465826537@qq.com>
2020-03-15 11:20:07 +0800

    fix page

---
+++ jcart-administration-front/route-page/router.js
@@ -0,0 +1,13 @@
+const routes = [
+    { path: '/product/search', component: ProductSearchRoutePage },
+    { path: '/product/update/:productId', component: ProductUpdateRoutePage },
+
+    { path: '/customer/search', component: CustomerSearchRoutePage },
+    { path: '/order/search', component: OrderSearchRoutePage },
+    { path: '/return/search', component: ReturnSearchRoutePage },
+    { path: '/administrator/index', component: AdministratorIndexRoutePage }
+];
+
+const router = new VueRouter({
+    routes: routes
+});
