b90914e709757094caf7c7de224a392a12833c2c
莜莜 <2465826537@qq.com>
2020-03-16 14:18:00 +0800

    fix page

--- jcart-administration-front/route-page/router.js
+++ jcart-administration-front/route-page/router.js
@@ -1,9 +1,39 @@
 const routes = [
     { path: '/product/search', component: ProductSearchRoutePage },
+    { path: '/product/create', component: ProductCreateRoutePage },
+    { path: '/product/update/:productId', component: ProductUpdateRoutePage },
+
+
     { path: '/customer/search', component: CustomerSearchRoutePage },
+    { path: '/customer/show/:customerId', component: CustomerShowRoutePage },
+    { path: '/address/index/:customerId', component: AddressIndexRoutePage },
+    { path: '/address/show/:addressId', component: AddressShowRoutePage },
+
     { path: '/order/search', component: OrderSearchRoutePage },
+    { path: '/order/show/:orderId', component: OrderShowRoutePage },
+
     { path: '/return/search', component: ReturnSearchRoutePage },
-    { path: '/administrator/index', component: AdministratorIndexRoutePage }
+
+
+    { path: '/administrator/index', component: AdministratorIndexRoutePage },
+    { path: '/administrator/updateprofile', component: AdministratorUpdateProfileRoutePage },
+    { path: '/administrator/create', component: AdministratorCreateRoutePage },
+    { path: '/administrator/update/:administratorId', component: AdministratorUpdateRoutePage },
+
+    {
+        path: '/return/edit/:returnId',
+        component: ReturnEditRoutePage,
+        children: [
+            {
+                path: 'show',
+                component: ReturnShowRoutePage
+            },
+            {
+                path: 'history',
+                component: ReturnHistoryIndexRoutePage
+            }
+        ]
+    }
    
 ];
 
