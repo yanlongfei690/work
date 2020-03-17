4ce669d868616af5eacd057e5c7fea27ee86b090
莜莜 <2465826537@qq.com>
2020-03-17 10:17:47 +0800

    add administrator administrator service

---
+++ jcart-administration-back/src/test/java/io/ylf/jcartadministrationback/service/impl/AdministratorServiceImplTest.java
@@ -0,0 +1,25 @@
+package io.ylf.jcartadministrationback.service.impl;
+
+import io.ylf.jcartadministrationback.po.Administrator;
+import io.ylf.jcartadministrationback.service.AdministratorService;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class AdministratorServiceImplTest {
+
+    @Autowired
+    private AdministratorService administratorService;
+
+    @Test
+    void getByUsername() {
+        String username = "admin";
+        Administrator administrator = administratorService.getByUsername(username);
+        assertNotNull(administrator);
+        username = "admin110";
+        administrator = administratorService.getByUsername(username);
+        assertNull(administrator);
+    }
+}
